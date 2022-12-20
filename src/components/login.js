afbccc31297be1562ad73bdfbde22fdc7bd3f878
shoamri joseph <shomarijoseph@gmail.com>
2022-12-20 15:12:37 -0500

    added onclick callback to login with google

--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,26 @@
-import React  from "react";
+import React from "react";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import "firebase/app";
+
+import { auth } from "../firebase";
 
 const Login = () => {
-    return (
-        <div id='login-page'>
-            <div id='login-card'>
-                <h2>Welcome to NaplesChat!</h2>
-
-                <div
-                className="login-button google"
-                
-                >
-                    <GoogleOutlined /> Sign in with Google
-                </div>
-                <br /> <br />
-
-                <div 
-                className="login-button facebook"
-                >
-                    <FacebookOutlined /> Sign in With Facebook
-                </div>
-            </div>
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <h2>Welcome to NaplesChat!</h2>
+        <div
+          className="login-button google"
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+          }
+        >
+          <GoogleOutlined /> Sign in with Google
         </div>
-    )
-}
+        <br /> <br />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
